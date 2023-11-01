1148e35db66a03c3b791563d9bf6f0abf8172953
Samiul Basir <118618515+Samiul-Basir-Creator@users.noreply.github.com>
2023-11-01 16:42:21 +0600

    Delete obh.py

--- obh.py
+++
@@ -1,9 +0,0 @@
-dec = int(input("Enter decimal number : "))
-
-octal = "{0:o}".format(dec)
-hexadecimal = "{0:x}".format(dec)
-binary ="{0:b}".format(dec)
-
-print("Octal : ",octal)
-print("Hexadecimal : ",hexadecimal)
-print("Binary : ",binary)
